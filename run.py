6fc9a2a450e5b11453f45cdead481bf517cacb30
prathyushabeerelli <2403a52125@sru.edu.in>
2026-05-05 11:59:33 +0530

    Update run.py to configure port for app.run
    Removed dotenv loading and added port configuration for app.run.

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
